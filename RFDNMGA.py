7ce3213d5e42c134d69049b8b56ba450ff7fd158
huxiaotaostasy <33438526+huxiaotaostasy@users.noreply.github.com>
2022-11-21 16:17:48 +0800

    Add files via upload

---
+++ RFDNMGA.py
@@ -0,0 +1,78 @@
+from torch import nn
+import torch
+from torch.nn import functional as F
+
+import block as B
+from MPModule import MPModule
+class RFDNBase(nn.Module):
+    def __init__(self, in_nc=3, nf=48, num_modules=4, out_nc=3, upscale=4, return_mid=False):
+        super(RFDNBase, self).__init__()
+        self.fea_conv = B.conv_layer(in_nc, nf, kernel_size=3)
+        self.B1 = B.RFDB(in_channels=nf)
+        self.B2 = B.RFDB(in_channels=nf)
+        self.B3 = B.RFDB(in_channels=nf)
+        self.B4 = B.RFDB(in_channels=nf)
+        self.c = B.conv_block(nf * num_modules, nf, kernel_size=1, act_type='lrelu')
+        self.LR_conv = B.conv_layer(nf, nf, kernel_size=3)
+        upsample_block = B.pixelshuffle_block
+        self.upsampler = upsample_block(nf, out_nc, upscale_factor=upscale)
+        self.return_mid = return_mid
+    def forward(self, input):
+        out_fea = self.fea_conv(input)
+        out_B1 = self.B1(out_fea)
+        out_B2 = self.B2(out_B1)
+        out_B3 = self.B3(out_B2)
+        out_B4 = self.B4(out_B3)
+        out_B = self.c(torch.cat([out_B1, out_B2, out_B3, out_B4], dim=1))
+        out_lr = self.LR_conv(out_B) + out_fea
+        output = self.upsampler(out_lr)
+
+        return output, out_lr
+
+class RFDNRefine(nn.Module):
+    def __init__(self, nf=50, scale=4):
+        super(RFDNRefine, self).__init__()
+        self.B1 = B.RFDB(in_channels=nf)
+        self.B2 = B.RFDB(in_channels=nf)
+        self.c = B.conv_block(nf * 2, nf, kernel_size=1, act_type='lrelu')
+        self.LR_conv = B.conv_layer(nf, nf, kernel_size=3)
+        self.conv1 = nn.Conv2d(nf, nf, 3, 1, 1)
+        self.conv2 = nn.Conv2d(nf, nf, 3, 1, 0)
+        upsample_block = B.pixelshuffle_block
+        self.upsampler = upsample_block(nf, 3, upscale_factor=scale)
+        self.lrelu = nn.LeakyReLU(negative_slope=0.1, inplace=True)
+    def replace_patch(self, coarse_img, patches, idx):
+        oB,oC,oH,oW=coarse_img.shape
+        pB,pC,pH,pW=patches.shape
+        coarse_img=coarse_img.view(oB,oC,oH//pH,pH,oW//pW,pW).permute(0,2,4,1,3,5)
+        coarse_img[idx[0],idx[1],idx[2]]=patches
+        coarse_img=coarse_img.permute(0,3,1,4,2,5).view(oB,oC,oH,oW)
+        return coarse_img
+
+    def forward(self, coarse_img, patches, idx):
+        coarse_img_1 = coarse_img.clone()
+        out_B1 = self.B1(patches)
+        out_B2 = self.B2(out_B1)
+        out_B = self.c(torch.cat([out_B1, out_B2], dim=1))
+        patches = self.LR_conv(out_B)+patches
+        patches = self.conv2(self.lrelu(self.conv1(patches)))
+        patches = self.upsampler(patches)
+        refine_img = self.replace_patch(coarse_img_1, patches, idx)
+        return refine_img
+
+class RFDNMGA(nn.Module):
+    def __init__(self, in_nc=3, nf=50, num_modules=4, out_nc=3, upscale=4, padding=1, size=4):
+        super(RFDNMGA, self).__init__()
+        self.base = RFDNBase(in_nc, nf, num_modules, out_nc, upscale, return_mid=True)
+        self.mask = MPModule(nf, padding=padding, size=size)
+        self.refine = RFDNRefine(nf, upscale)
+    def forward(self,input):
+        coarse_img, out_lr = self.base(input)
+        patches, idx, mask = self.mask(out_lr)
+        refine_img = self.refine(coarse_img, patches, idx)
+        return refine_img
+if __name__ == '__main__':
+    x = torch.randn((2, 3, 64, 64))
+    net = RFDNMGA()
+    sr = net(x)
+    print(sr.shape)
